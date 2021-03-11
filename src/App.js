12d1c13f48640a8e50e58227bab51f12a3c87eca
abiagyemang <abigailagyemang16@gmail.com>
2021-03-11 21:37:12 +1200

    assignment-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import MyFirstComponent from './myfifthcomponent';
+import MySecondComponent from './mysecondcomponent';
+import MyThirdComponent from './mythirdcomponent';
+import MyFourthComponent from './myfourthcomponent';
+import myfifthcomponent from './myfifthcomponent';
+import MySixthComponent from './mysixthcomponent';
+import MySeventhComponent from './myseventhcomponent';
+import MyEightComponent from './myeightcomponent';
+import MyNinthComponent from './myninthcomponent';
+import MyTenthComponent from './mytenthcomponent';
+import MyFifthComponent from './myfifthcomponent';
 
 // import { getDefaultNormalizer } from '@testing-library/dom';
 // import MasterComponent from  './MasterComponent';
 // import ParentComponent from './parentcomponent';
-import DashboardComponent from './Trial1/dashboardcomponent';
-import NotificationComponent from "./Trial1/notificationcomponent"
+// import NotificationComponent from "./Trial1/notificationcomponent"
+// import DashboardComponent from './Trial1/dashboardcomponent';
+
 
 function App() {
   return (
     <div>
-     {/* <MasterComponent name="1" height="45"/>
-     <MasterComponent name="2" height="67"/>
-     <MasterComponent name="3" height="34"/>
-     <MasterComponent name="4" height="30"/> */}
+      <MyFirstComponent/>
+      <MySecondComponent/>
+      <MyThirdComponent/>
+      <MyFourthComponent/>
+      <MyFifthComponent/>
+      <MySixthComponent/>
+      <MySeventhComponent/>
+      <MyEightComponent/>
+      <MyNinthComponent/>
+      <MyTenthComponent/>
 
 
-     {/* <ParentComponent Name= "abena" gender= "female" Age= "26"email= "abigailagyemang16@gmail.com" /> */}
+
+     {/* <ParentComponent Name= "abena" gender= "female" Age= "26"email= "abigailagyemang16@gmail.com" />
      <DashboardComponent dashboardFName="Abigail"
       dashboardLName="Agyemang" 
       dashboardEmail="abigailagyemang16@gmail.com" 
       dashboardNumber="0201296609"
-      dashboardDateOfBirth="06/08/1994"/>
+      dashboardDateOfBirth="06/08/1994"/> */}
 
     </div>
   );
